4846df9e956825ad3eef84098259f8f0c5c55ed8
aditya0909k <85547196+aditya0909k@users.noreply.github.com>
2022-04-22 19:20:27 -0500

    fix humanplayer range

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -9,20 +9,28 @@ public class HumanPlayer extends APlayer{
 
     @Override
     public int selectRowValue(int range) {
+        range-=1;
         System.out.println("Please enter a valid row number from 0 to " + range);
         int row;
         do {
             row = input.nextInt();
-        } while (row < 0 && row > range);
+            if (row < 0 || row > range) {
+                System.out.println("Invalid move! Enter a number from 0 to " + range);
+            }
+        } while (row < 0 || row > range);
         return row;
     }
     @Override
     public int selectColValue(int range) {
+        range-=1;
         System.out.println("Please enter a valid column number from 0 to " + range);
         int col;
         do {
             col = input.nextInt();
-        } while (col < 0 && col > range);
+            if (col < 0 || col > range) {
+                System.out.println("Invalid move! Enter a number from 0 to " + range);
+            }
+        } while (col < 0 || col > range);
         return col;
     }
 }
